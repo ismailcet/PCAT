d17c4eb9ebd2eebbbdd2244f2dd319cca236db6d
ismailcet <ismailcet3@gmail.com>
2022-11-08 01:07:56 +0300

    ExpressJs is added.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log("Function Working");
-};
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const photo = {
+    id: 1,
+    name: "photoName",
+    desc: "Photo Desc",
+  };
+  response.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} Portunda Başlatıldı`);
+});
