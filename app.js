4f0cf0313fb852217d97af204a0fda0e24799ece
ismailcet <ismailcet3@gmail.com>
2022-11-08 17:14:20 +0300

    Add TemplatEngine

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
+
 const app = express();
 
+//Tempalate Engine
+app.set("view engine", "ejs");
+
+//MiddleWares
 app.use(express.static("public"));
 
+//Queries
 app.get("/", (request, response) => {
-  response.sendFile(path.resolve(__dirname, "temp/index.html"));
+  response.render("index");
+});
+
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+
+app.get("/add", (request, response) => {
+  response.render("add");
 });
 
 const port = 3000;
