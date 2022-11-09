5d4a80c1ac15dbb1d741864d1febd8a0427e7444
ismailcet <ismailcet3@gmail.com>
2022-11-09 22:53:17 +0300

    Connected with database and Add İmage info in database

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const ejs = require("ejs");
 
+const { Photo } = require("./models/Photo");
+
 const app = express();
+
+//Connect To DB
+mongoose.connect("mongodb://localhost/pcat-text-db");
 
 //Tempalate Engine
 app.set("view engine", "ejs");
 
 //MiddleWares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Queries
-app.get("/", (request, response) => {
-  response.render("index");
+app.get("/", async (request, response) => {
+  const photos = await Photo.find({});
+  response.render("index", {
+    photos: photos,
+  });
 });
 
 app.get("/about", (request, response) => {
@@ -21,6 +32,11 @@ app.get("/about", (request, response) => {
 
 app.get("/add", (request, response) => {
   response.render("add");
+});
+//Post Method
+app.post("/photos", async (request, response) => {
+  await Photo.create(request.body);
+  response.redirect("/");
 });
 
 const port = 3000;
