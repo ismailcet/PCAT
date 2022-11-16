e80d4d432c35c14fb18e13c12f09810ac9b52e82
ismailcet <ismailcet3@gmail.com>
2022-11-17 01:40:09 +0300

    Photo Added , Uploaded , Edited Pages are added this project.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+//Express File Upload Dosyamızı Require ile tanımladık
+const fileUpload = require("express-fileupload");
+
+const methodOverride = require("method-override");
 const path = require("path");
 const ejs = require("ejs");
+//Dosya Kontrol Sitemimizi Ekledik
+const fs = require("fs");
 
 const { Photo } = require("./models/Photo");
+const { PassThrough } = require("stream");
 
 const app = express();
 
@@ -17,10 +24,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload()); //File Upload middleWaresi Ekledik
+app.use(methodOverride("_method"));
 
 //Queries
 app.get("/", async (request, response) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort("-dateCreated"); //DateCreated a göre sıralama yapmasını söyledik
   response.render("index", {
     photos: photos,
   });
@@ -35,8 +44,30 @@ app.get("/add", (request, response) => {
 });
 //Post Method
 app.post("/photos", async (request, response) => {
-  await Photo.create(request.body);
-  response.redirect("/");
+  // await Photo.create(request.body);
+  // response.redirect("/");
+  // console.log(request.files.image);
+
+  const uploadDir = "public/uploads"; //Upload Dosyamızı Tanımlıyoruz
+
+  //Dosyamız Yok ise uploads dosyamızı oluşturuyoruz
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  //Upload Edilecek Doyamızın Bilgilerini ALıyoruz
+  let sampleFile = request.files.image;
+  //Upload Edilen dosyaların kaydedileceği dosyamızı tanıtıyoruz
+  let uploadImage = __dirname + "/public/uploads/" + sampleFile.name;
+
+  //SampleFile içindeki move(mv) fonksiyonu kullanarak kaydediyoruz
+  sampleFile.mv(uploadImage, async () => {
+    await Photo.create({
+      ...request.body,
+      image: "/uploads/" + sampleFile.name,
+    });
+    response.redirect("/");
+  });
 });
 
 app.get("/photos/:id", async (request, response) => {
@@ -45,6 +76,24 @@ app.get("/photos/:id", async (request, response) => {
   response.render("video-page", {
     photo,
   });
+});
+
+app.get("/photo/edit/:id", async (request, response) => {
+  const photo = await Photo.findOne({ _id: request.params.id });
+
+  response.render("edit", {
+    photo,
+  });
+});
+
+app.put("/photos/:id", async (request, response) => {
+  const photo = await Photo.findById(request.params.id);
+
+  photo.title = request.body.title;
+  photo.description = request.body.description;
+
+  photo.save();
+  response.redirect(`/photos/${request.params.id}`);
 });
 
 const port = 3000;
